993806373df72390704aeb6320c43ccd884bb572
Daniele Fea <fea.daniele@gmail.com>
2023-03-03 22:08:04 +0100

    Caricamento iniziale dei file

---
+++ js/dati.js
@@ -0,0 +1,110 @@
+var mittenti =[
+    1,
+    0,
+    1,
+    0,
+
+    0,
+    2,
+    0,
+    2,
+    0,
+
+    3,
+    0,
+    3,
+    0,
+    3,
+
+    4,
+    0,
+    4,
+    4,
+    0,
+    4,
+
+    0
+];
+
+var destinatari = [
+    0,
+    1,
+    0,
+    1,
+
+    2,
+    0,
+    2,
+    0,
+    2,
+
+    0,
+    3,
+    0,
+    3,
+    0,
+
+    0,
+    4,
+    0,
+    0,
+    4,
+    0,
+
+    5
+];
+
+var messaggi = [
+    "Ciao, come stai?",
+    "Ciao &#128512;! Tutto bene, tu?",
+    "Più o meno, fra poco ho una verifica &#128532;",
+    "&#128549;",
+
+    "Ciao Maria, come è stato il carnevale ad Ivrea?",
+    "&#128525; Bellissimo!! &#128151; &#128151;",
+    "&#128516; Hai tirato anche tu qualche arancia?",
+    "Noooo, sono stata dietro Marco tutto il tempo...",
+    "Ah, quindi è venuto anche Marco...",
+
+    "Ciao Ut!! &#128516; &#128516;",
+    "Ehi &#128527;",
+    "Sei già arrivato a scuola?",
+    "Sì, sono in classe",
+    "ohhh &#128519;, come mai non sei passato a salutare?",
+
+    "Sei arrivato?",
+    "Sì, tranquilla",
+    "Hai ordinato il pranzo??",
+    "Ci sei?????",
+    "Fra poco vado...",
+    "Ma non sei a lezione! &#128545;",
+
+    "Ciao Gigi, oggi a che ora arrivi?"
+];
+
+var nomeUtenti = [
+    "Ut",
+    "Mario",
+    "Maria",
+    "Elena",
+    "Ginevra",
+    "Luigi"
+];
+
+var cognomeUtenti = [
+    "Login",
+    "Rossi",
+    "Bianchi",
+    "Strada",
+    "Fiore",
+    "Silente"
+];
+
+var genereUt = [
+    "m",
+    "m",
+    "f",
+    "f",
+    "f",
+    "m"
+];
---
+++ js/script.js
@@ -0,0 +1,22 @@
+/*
+    TESTO DELLA VERIFICA DI TPSIT
+    Viene richiesto di modificare i file html e js in modo tale da:
+
+    - visualizzare DINAMICAMENTE gli utenti nell'aside (barra laterale sinistra) 
+        # ogni utente è un nuovo LI
+        # ogni utente in base al genere dovrà essere raffigurato tramite l'icona corretta
+        # sotto l'icona dovrà essere rafficurato il nome con l'iniziale maiuscola del nome e l'iniziale maiuscola del cognome
+        # il cognome dovrà essere troncato e seguito da . (come si vede nel file originale)
+
+    - aggiornare automaticamente l'header della sezione nel momento in cui clicco su un utente dell'aside
+        # modificare l'icona
+        # modificare il nome e cognome
+        # aggiornare l'ora a proprio piacimento (PER IL MASSIMO DEL PUNTEGGIO: creare un array parallelo con le ore)
+    
+    - aggiungere DINAMICAMENTE I MESSAGGI
+        #Non importa se si utilizza ut1 per l'utente 0 o ut2, l'importante è la coerenza
+
+    - PER IL 10. Gestite il bottone di invio in basso 
+        #se il campo di testo non è vuoto aggiungere il messaggio in coda agli altri del personaggio selezionato
+
+*/
