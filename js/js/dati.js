bccce28b84256a55e5f7af967731383be9d1e42a
leobertolotti1806 <120721601+leobertolotti1806@users.noreply.github.com>
2023-03-04 13:18:28 +0100

    Aggiunta extra e ora ogni messaggio inviato

---
+++ js/js/dati.js
@@ -0,0 +1,119 @@
+var mittenti =[
+    1,
+    0,
+    1,
+    0,
+
+    0,
+    2,
+    0,
+    2,
+    0,
+
+    3,
+    0,
+    3,
+    0,
+    3,
+
+    4,
+    0,
+    4,
+    4,
+    0,
+    4,
+
+    0
+];
+
+var destinatari = [
+    0,
+    1,
+    0,
+    1,
+
+    2,
+    0,
+    2,
+    0,
+    2,
+
+    0,
+    3,
+    0,
+    3,
+    0,
+
+    0,
+    4,
+    0,
+    0,
+    4,
+    0,
+
+    5
+];
+
+var messaggi = [
+    "Ciao, come stai?",
+    "Ciao &#128512;! Tutto bene, tu?",
+    "Più o meno, fra poco ho una verifica &#128532;",
+    "&#128549;",
+
+    "Ciao Maria, come è stato il carnevale ad Ivrea?",
+    "&#128525; Bellissimo!! &#128151; &#128151;",
+    "&#128516; Hai tirato anche tu qualche arancia?",
+    "Noooo, sono stata dietro Marco tutto il tempo...",
+    "Ah, quindi è venuto anche Marco...",
+
+    "Ciao Ut!! &#128516; &#128516;",
+    "Ehi &#128527;",
+    "Sei già arrivato a scuola?",
+    "Sì, sono in classe",
+    "ohhh &#128519;, come mai non sei passato a salutare?",
+
+    "Sei arrivato?",
+    "Sì, tranquilla",
+    "Hai ordinato il pranzo??",
+    "Ci sei?????",
+    "Fra poco vado...",
+    "Ma non sei a lezione! &#128545;",
+
+    "Ciao Gigi, oggi a che ora arrivi?"
+];
+
+var nomeUtenti = [
+    "Ut",   //0
+    "Mario",//1
+    "Maria",//2
+    "Elena",//3
+    "Ginevra",//4
+    "Luigi" //5
+];
+
+var accesso = [
+    "10:54",
+    "17:22",
+    "0:44",
+    "16:30",
+    "20:10",
+    "7:02"
+]
+
+var cognomeUtenti = [
+    "Login", //0
+    "Rossi", //1
+    "Bianchi", //2
+    "Strada", //3
+    "Fiore", //4
+    "Silente" //5
+];
+
+var genereUt = [
+    "m", //0
+    "m", //1
+    "f", //2
+    "f", //3
+    "f", //4
+    "m" //5
+];
---
+++ js/js/script.js
@@ -0,0 +1,123 @@
+/*
+    TESTO DELLA VERIFICA DI TPSIT
+    Viene richiesto di modificare i file html e js in modo tale da:
+
+    - visualizzare DINAMICAMENTE gli utenti nell'aside (barra laterale sinistra) 
+        # ogni utente è un nuovo LI
+        # ogni utente in base al genere dovrà essere raffigurato tramite l'icona corretta
+        # sotto l'icona dovrà essere rafficurato il nome con l'iniziale maiuscola del nome e l'iniziale maiuscola del cognome
+        # il cognome dovrà essere troncato e seguito da . (come si vede nel file originale)
+
+    - aggiornare automaticamente l'header della sezione nel momento in cui clicco su un utente dell'aside
+        # modificare l'icona
+        # modificare il nome e cognome
+        # aggiornare l'ora a proprio piacimento (PER IL MASSIMO DEL PUNTEGGIO: creare un array parallelo con le ore)
+    
+    - aggiungere DINAMICAMENTE I MESSAGGI
+        #Non importa se si utilizza ut1 per l'utente 0 o ut2, l'importante è la coerenza
+
+    - PER IL 10. Gestite il bottone di invio in basso 
+        #se il campo di testo non è vuoto aggiungere il messaggio in coda agli altri del personaggio selezionato
+
+*/
+
+
+let listUser = document.querySelector("aside > ul");
+let header = document.querySelector("section > header");
+let section = document.querySelector("section > section");
+
+
+listUser.innerHTML = "";
+
+for (let i = 1; i < nomeUtenti.length; i++) {
+
+    //  CARICAMENTO ASIDE
+    let li = document.createElement("li");
+    let div = document.createElement("div");
+    div.classList.add("material-symbols-outlined", "icone");
+
+    if (genereUt[i] == "m")
+        div.textContent = "face";
+    else
+        div.textContent = "face_3";
+
+    li.appendChild(div);
+    li.innerHTML += nomeUtenti[i] + " " + cognomeUtenti[i][0] + ".";
+    listUser.appendChild(li);
+
+    li.addEventListener("click", function () {
+        //click di un utente
+
+        let face = header.querySelector("div");
+        let userName = document.getElementById("divNome");
+        let ultimoAccesso = document.getElementById("divUltimoMes");
+
+        if (genereUt[i] == "m")
+            face.textContent = "face_3";
+        else
+            face.textContent = "face";
+
+        userName.textContent = nomeUtenti[i] + " " + cognomeUtenti[i];
+        ultimoAccesso.textContent = "oggi alle " + accesso[i];
+
+
+        /*
+           MESSAGGI IN NERO SONO QUELLI INVIATI DALL'UTENTE
+           QUELLI IN BIANCO SONO QUELLI RICEVUTI DALL'UTENTE
+        */
+
+        //classe ut1 ==> BIANCO - RICEVUTI
+        //classe ut2 ==> NERI - INVIATI
+
+        //  creazione messaggi
+        let numeroMess = 0, offset = 0;
+
+        //trova messaggio
+        do
+            offset++;
+        while (mittenti[offset] != i && destinatari[offset] != i);
+
+        //trova quanti messaggi stampare
+        do
+            numeroMess++;
+        while (mittenti[offset + numeroMess] <= i && destinatari[offset + numeroMess] <= i);
+
+
+        section.innerHTML = "";
+
+        for (let j = 0; j < numeroMess; j++) {
+
+            let article = document.createElement("article");
+            article.classList.add("mes");
+
+            if (mittenti[j] == 0)
+                article.classList.add("ut1");
+            else
+                article.classList.add("ut2");
+
+            article.innerHTML = messaggi[offset + j];
+
+            section.appendChild(article);
+        }
+
+    });
+
+}
+
+let input = document.querySelector("input");
+input.addEventListener("change", function () {
+
+    let article = document.createElement("article");
+
+    article.classList.add("mes", "ut2");
+
+    article.innerHTML = this.value;
+
+    section.appendChild(article);
+
+    let span = document.createElement("span");
+    let data = new Date();
+    span.innerHTML = data.getHours() + ":" + data.getMinutes() + ":" + data.getSeconds();
+    console.log(Date.now)
+    article.appendChild(span);
+});
